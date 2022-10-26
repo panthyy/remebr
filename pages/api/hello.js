9f83ec689a2225eae747ff8c16caca42d2146c37
panthyy <fahad.alhatshami@hotmail.com>
2022-10-27 01:47:51 +0200

    launch

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const config = {
+  runtime: "experimental-edge",
+};
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
